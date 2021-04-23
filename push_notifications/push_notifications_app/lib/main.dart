6890ccc46c1daa33e2baac90e7c46053c212d3d5
Danthology96 <daniel.andres.novillo@gmail.com>
2021-04-22 22:44:20 -0500

    Versión funcional con error en el build (Null), esperando solución.

--- push_notifications/push_notifications_app/lib/main.dart
+++ push_notifications/push_notifications_app/lib/main.dart
@@ -1,19 +1,95 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:push_notifications_app/src/pages/home_page.dart';
 import 'package:push_notifications_app/src/pages/mensaje_page.dart';
+import 'package:push_notifications_app/src/providers/push_notifications_provider.dart';
 
-void main() => runApp(MyApp());
+PushNotificationsProvider provider = new PushNotificationsProvider();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-class MyApp extends StatelessWidget {
+  // Set the background messaging handler early on, as a named top-level function
+  FirebaseMessaging.onBackgroundMessage(
+      provider.firebaseMessagingBackgroundHandler);
+
+  /// Create an Android Notification Channel.
+  ///
+  /// We use this channel in the `AndroidManifest.xml` file to override the
+  /// default FCM channel to enable heads up notifications.
+  await provider.flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(PushNotificationsProvider.channel);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  void initState() {
+    provider.getToken();
+    super.initState();
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    provider.flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null) {
+        provider.flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                PushNotificationsProvider.channel.id,
+                PushNotificationsProvider.channel.name,
+                PushNotificationsProvider.channel.description,
+                // TODO add a proper drawable resource to android, for now using
+                //      one that already exists in example app.
+                icon: 'launch_background',
+              ),
+            ));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'home',
-      routes: {
-        'home': (BuildContext c) => HomePage(),
-        'mensaje': (BuildContext c) => MensajePage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Material App',
+            initialRoute: 'home',
+            routes: {
+              'home': (BuildContext c) => HomePage(),
+              'mensaje': (BuildContext c) => MensajePage(),
+            },
+          );
+        }
+        //Para el error de build
+        return Container(
+          width: 0.0,
+          height: 0.0,
+        );
       },
     );
   }
---
+++ push_notifications/push_notifications_app/lib/src/providers/push_notifications_provider.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class PushNotificationsProvider {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  /// Create a [AndroidNotificationChannel] for heads up notifications
+  static const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  /// Initialize the [FlutterLocalNotificationsPlugin] package.
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+    // If you're going to use other Firebase services in the background, such as Firestore,
+    // make sure you call `initializeApp` before using other Firebase services.
+    await Firebase.initializeApp();
+
+    print("Handling a background message: ${message.messageId}");
+  }
+
+  getToken() async {
+    String token = await FirebaseMessaging.instance.getToken();
+    print(token);
+  }
+}
